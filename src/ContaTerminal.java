84cafb30501ba3208ab868bf93a7bf0ceca88973
robertlacerda <lacerdarobert793@gmail.com>
2026-03-25 13:06:00 -0300

    adicionei o TODO: no início do código

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class ContaTerminal {
 
     public static void main(String[] args) {
+        /**
+         * TODO: Conhecer e importar a classe Scanner
+         * Exibir as mensagens para o nosso usuário
+         * Obter pela scanner os valores digitados no terminal
+         * Exibir a mensagem conta criada
+         */
+
         // Atributos
         int numero = 0;
         String agencia = "";
